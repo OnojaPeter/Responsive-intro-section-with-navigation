65b6ec0b2b5343b6d37c0d90e7f5c8b8c6b4c496
OnojaPeter <onojapeter90@gmail.com>
2023-07-16 01:47:53 +0800

    first submit
    first date: 15 July 2023

---
+++ index.js
@@ -0,0 +1,9 @@
+function openNav(){
+  document.getElementById("secondnav").style.width = "60%";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.6)";
+}
+
+function closeNav(){
+  document.getElementById("secondnav").style.width = "0";
+  document.body.style.backgroundColor = "white";
+}
